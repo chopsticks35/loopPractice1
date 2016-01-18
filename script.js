630ccf21018e2e9b80162a8d280f211de14e20bf
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-01-18 10:55:23 -0700

    final

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 
-for ( var i=0; i<=500; i=i+100 ) {
+// for ( var i=0; i<=500; i=i+100 ) {
     
-    console.log(i);
+//     console.log(i);
+// }
+
+
+// for (var j=1; j<=64; j=j+j) {
+
+// 	console.log(j);
+// }
+
+// for (var k=1; k<4; k++) {
+
+// 	if (k=1){
+// 		console.log(k);
+// 		console.log(k);
+// 		console.log(k);
+
+// 	}
+
+// 	else if (k=2){
+// 		console.log(k);
+// 		console.log(k);
+// 		console.log(k)
+// 	}
+
+// 	else {
+// 		console.log(k);
+// 		console.log(k);
+// 		console.log(k)
+// 	}
+// }
+
+// 
+
+// for (i=0; i<=10; i=i+2) {
+// 	console.log(i);
+// 
+
+
+// 
+// 
+for(i=1; i<=3; i++){
+	for (j=0; j<=3; j++){
+		console.log(i);
+	}
 }
-
-
-for (var j=1; j<=64; j=j+j) {
-
-	console.log(j);
-}
-
-for (var k=1; )
